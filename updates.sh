499e780794233f8dd30650a14cdd1be61fdf21cf
Viraj_Gotmare <virajchronos@gmail.com>
2026-09-04 10:17:04 +0530

    Add w1 - d1,d2,d4 solutions

--- updates.sh
+++ updates.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+msg="$1"
+
+if [ -z "$msg" ]; then
+    echo "Enter your commit message: "
+    read msg
+fi
+
+git add .
+git commit -m "$msg"
+
 git fetch upstream
+
 git checkout main
+
 git merge upstream/main
+
 git push origin main
-git pull
+
 git checkout dev
+
 git merge main
+
 git push origin dev
+
